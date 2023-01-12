bbab3ef531c000737bf7292b600d470c831d638b
Abel5173 <86670708+Abel5173@users.noreply.github.com>
2023-01-12 21:49:04 +0300

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,17 @@
+#!/usr/bin/python3
+def get_ponderated_promedium(scores):
+    list_promediums = list(map(lambda a: a[0] * a[1], scores))
+    return sum(list_promediums)
+
+
+def get_sum_weight(scores):
+    list_weights = list(map(lambda a: a[1], scores))
+    return sum(list_weights)
+
+
 def weight_average(my_list=[]):
     if len(my_list) == 0:
-      return 0
+        return 0
     else:
-       x = 0
-       y = 0
-      for i in my_list:
-        x += i[0]*i[1]
-        y += i[1]
-      return x/y
+        average = get_ponderated_promedium(my_list) / get_sum_weight(my_list)
+        return average
