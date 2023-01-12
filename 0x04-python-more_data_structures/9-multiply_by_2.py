146cd16c7a8ab0873c34de6b51a9869873223237
Abel5173 <86670708+Abel5173@users.noreply.github.com>
2023-01-12 21:17:30 +0300

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    multiple = {}
-    for i in a_dictionary:
-        multiple[i] = a_dictionary[i] * 2
+    multiple = {x:a_dictionary[x]*2 for x in a_dictionary }
+#     for i in a_dictionary:
+#         multiple[i] = a_dictionary[i] * 2
     return multiple
